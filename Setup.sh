e827c729f13c55e64f14dd8c534fd2dafd808c26
Fingers <sjmshelton@proton.me>
2026-09-02 16:57:18 +1200

    Swap to helium

--- Setup.sh
+++ Setup.sh
@@ -3,7 +3,7 @@ set -e
 
 cd ~ && sudo pacman -S --needed git base-devel
 cd /tmp && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && yay --version
-yay -S brave-bin
+yay -S helium-browser-bin
 
 sudo pacman -Sy fish ripgrep unzip grim slurp github-cli cliphist wl-clipboard glfw hyprlock vulkan-devel tree-sitter-cli hyprpaper noto-fonts noto-fonts-emoji noto-fonts-extra ttf-liberation ttf-jetbrains-mono-nerd ttf-dejavu cppcheck git bluetui waybar discord spotify-launcher rofi htop neovim kitty fastfetch ninja premake cmake lua lua-language-server yazi llvm clang lldb base-devel
 sudo pacman -Rns dolphin
