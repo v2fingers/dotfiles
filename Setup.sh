d7bc2b6e15b26678f7f07e4dee694ea8dc7f57ad
Fingers <sjmshelton@proton.me>
2026-09-02 14:56:14 +1200

    Added ripgrep

--- Setup.sh
+++ Setup.sh
@@ -5,7 +5,7 @@ cd ~ && sudo pacman -S --needed git base-devel
 cd /tmp && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && yay --version
 yay -S brave-bin
 
-sudo pacman -Sy unzip grim slurp github-cli cliphist wl-clipboard glfw hyprlock vulkan-devel tree-sitter-cli hyprpaper noto-fonts noto-fonts-emoji noto-fonts-extra ttf-liberation ttf-jetbrains-mono-nerd ttf-dejavu cppcheck git bluetui waybar discord spotify-launcher rofi btop neovim kitty fastfetch ninja premake cmake lua lua-language-server yazi llvm clang lldb base-devel
+sudo pacman -Sy ripgrep unzip grim slurp github-cli cliphist wl-clipboard glfw hyprlock vulkan-devel tree-sitter-cli hyprpaper noto-fonts noto-fonts-emoji noto-fonts-extra ttf-liberation ttf-jetbrains-mono-nerd ttf-dejavu cppcheck git bluetui waybar discord spotify-launcher rofi btop neovim kitty fastfetch ninja premake cmake lua lua-language-server yazi llvm clang lldb base-devel
 sudo pacman -Rns dolphin htop
 sudo pacman -Rns $(pacman -Qdtq)
 
